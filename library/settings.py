912585233d3a717773c1ff0da338531f8231583e
Sandrine <svm@cesar.org.br>
2022-06-30 19:59:04 -0300

    ajustando configurações de imagens

--- library/settings.py
+++ library/settings.py
@@ -146,4 +146,3 @@ AWS_STORAGE_BUCKET_NAME= "livrariadjango"
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = "us-east-2"
